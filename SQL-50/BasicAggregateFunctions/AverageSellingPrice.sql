6a6c1504947c3e09bafc623e808175db683b58f9
Atreyi Biswas <atreyi.biswas@outlook.com>
2026-06-28 13:55:19 +0530

    Update AverageSellingPrice.sql

--- SQL-50/BasicAggregateFunctions/AverageSellingPrice.sql
+++ SQL-50/BasicAggregateFunctions/AverageSellingPrice.sql
@@ -1,1 +1,10 @@
-
+SELECT Prices.product_id,
+  ROUND(
+    COALESCE(
+      SUM(Prices.price * UnitsSold.units) / NULLIF(SUM(UnitsSold.units), 0),
+    0),
+  2) AS average_price
+FROM Prices LEFT JOIN UnitsSold
+ON Prices.product_id = UnitsSold.product_id
+AND UnitsSold.purchase_date BETWEEN Prices.start_date AND Prices.end_date
+GROUP BY Prices.product_id;
