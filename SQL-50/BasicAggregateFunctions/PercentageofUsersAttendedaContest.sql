61e5a752b91211fba158f56b517a84dafeb89e05
Atreyi Biswas <atreyi.biswas@outlook.com>
2026-08-17 20:19:05 +0530

    update

--- SQL-50/BasicAggregateFunctions/PercentageofUsersAttendedaContest.sql
+++ SQL-50/BasicAggregateFunctions/PercentageofUsersAttendedaContest.sql
@@ -1,1 +1,7 @@
-
+SELECT 
+    Register.contest_id,
+    ROUND(COUNT(Register.user_id) * 100.0 / COUNT(Users.user_id), 2) AS percentage
+FROM Users
+JOIN Register
+ON Users.user_id = Register.user_id
+GROUP BY Register.contest_id;
